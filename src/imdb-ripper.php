fdaa0d6ba077db238b82bd15fb52679e8dbd44b5
petermrg <petermrg@ymail.com>
2013-04-01 19:14:02 +0200

    Added country information

--- src/imdb-ripper.php
+++ src/imdb-ripper.php
@@ -95,18 +95,31 @@ class IMDbRipper {
         $data = array();
 
         $data['title'] = $this->getValue($xpath->query('//*[@id="overview-top"]/h1/span[1]'));
+
         $data['image'] = $this->fullImage($this->getAttr(
             $xpath->query('//*[@id="img_primary"]/div/a/img')->item(0), 'src')
         );
         $data['original_title'] = $this->getValue($xpath->query('//*[@id="overview-top"]/h1/span[3]/text()'));
+
+        $data['country'] = $this->getValues($xpath->query('//*[@id="titleDetails"]/div[2]/a'));
+
         $data['year'] = $this->getValue($xpath->query('//*[@id="overview-top"]/h1/span[2]/a'));
+
         $data['duration'] = $this->getValue($xpath->query('//*[@id="overview-top"]/div[2]/time'));
+
         $data['score'] = $this->getValue($xpath->query('//*[@id="overview-top"]/div[3]/div[1]'));
-        $data['content_rating'] = $this->getAttr($xpath->query('//*[@id="overview-top"]/div[2]/span[1]')->item(0), 'title');
+
+        $data['content_rating'] = $this->getAttr(
+            $xpath->query('//*[@id="overview-top"]/div[2]/span[1]')->item(0), 'title'
+        );
         $data['director'] = $this->getLinks($xpath->query('//*[@id="overview-top"]/div[4]/a'));
+
         $data['writers'] = $this->getLinks($xpath->query('//*[@id="overview-top"]/div[5]/a'));
+
         $data['stars'] = $this->getLinks($xpath->query('//*[@id="overview-top"]/div[6]/a'));
+
         $data['genres'] = $this->getValues($xpath->query('//*[@id="titleStoryLine"]/div[4]/a'));
+
         $data['cast'] = $this->getTable($xpath, '//*[@id="titleCast"]/table/tr', array(
             'name' => function($xpath, $tr, $n) {
                 $item = $xpath->query($tr.'['.$n.']/td[2]/a');
